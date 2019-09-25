4967200a02005e4e2abfabe630362ad7c00f7b49
Ryan Burnette <ryan.burnette@gmail.com>
2019-09-25 10:20:49 -0400

    intial

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(req) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'Bearer'
+  ) {
+    return req.headers.authorization.split(' ')[1];
+  }
+};
